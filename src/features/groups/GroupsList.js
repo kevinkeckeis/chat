314378a53a0a25698b3d707f182ea9323d4a4380
Kevin Keckeis <35276744+kevinkeckeis@users.noreply.github.com>
2022-06-27 22:15:20 +0200

    add groups/home/stories structure

---
+++ src/features/groups/GroupsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import ContactsList from '../contacts/ContactsList';
+
+const GroupsList = () => {
+  return <h1>groups</h1>;
+};
+
+export default GroupsList;
