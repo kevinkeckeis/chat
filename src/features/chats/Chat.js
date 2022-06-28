400842bc66ea2cdfc51d0d8eac6a9c3629ae38f2
Kevin Keckeis <35276744+kevinkeckeis@users.noreply.github.com>
2022-06-28 17:27:03 +0200

    make chat move bottom after input

--- src/features/chats/Chat.js
+++ src/features/chats/Chat.js
@@ -1,16 +1,36 @@
+import { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectMessagesByUserId } from './chatsSlice';
 import ChatItem from '../../components/ChatItem';
+import ChatInput from '../../components/ChatInput';
 
 const Chat = () => {
   const { contactId } = useParams();
   const chatsByUserId = useSelector(selectMessagesByUserId(contactId));
+  const message = useSelector((state) => state.chats);
+
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [message]);
+
+  scrollToBottom();
 
   const chats = chatsByUserId.map((chat) => (
     <ChatItem key={chat.id} chat={chat} />
   ));
-  return <div className='flex flex-col'>{chats}</div>;
+  return (
+    <div className='flex flex-col p-3'>
+      {chats}
+      {<ChatInput contactId={contactId} />}
+      <div ref={messagesEndRef}></div>
+    </div>
+  );
 };
 
 export default Chat;
