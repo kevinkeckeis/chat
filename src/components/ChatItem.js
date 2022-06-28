543cdc42956c44186d99578dcfbb7a32fb6e3d0b
Kevin Keckeis <35276744+kevinkeckeis@users.noreply.github.com>
2022-06-28 17:31:56 +0200

    add ChatItem component

--- src/components/ChatItem.js
+++ src/components/ChatItem.js
@@ -6,38 +6,36 @@ import { formatDistance } from 'date-fns';
 const ChatItem = ({ chat }) => {
   const fullname = useSelector(selectFullnameByContactId(chat.userId));
 
-  const message = () => {
-    if (!chat.isResponse) {
-      return (
-        <div className='flex'>
-          <div className='m-2 p-2 border rounded-lg basis-9/12 flex-initial'>
-            <p className='text-sm font-bold'>{fullname}</p>
-            <p className='text-lg'>{chat.message}</p>
-            <p className='text-sm italic text-gray-500'>
-              {formatDistance(chat.sendAt, new Date(), {
-                addSuffix: true,
-              })}
-            </p>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className='flex justify-end'>
-          <div className='m-2 p-2 border rounded-lg basis-9/12 flex-initial self-end'>
-            <p className='text-lg'>{chat.message}</p>
-            <p className='text-sm italic text-gray-500'>
-              {formatDistance(chat.sendAt, new Date(), {
-                addSuffix: true,
-              })}
-            </p>
-          </div>
-        </div>
-      );
-    }
-  };
+  const ChatMessageFrom = () => (
+    <div className='flex'>
+      <div className='m-2 p-2 border rounded-lg basis-9/12 flex-initial'>
+        <p className='text-sm font-bold'>{fullname}</p>
+        <p className='text-lg'>{chat.message}</p>
+        <p className='text-xs italic text-gray-500 float-right'>
+          {formatDistance(chat.sendAt, new Date(), {
+            addSuffix: true,
+          })}
+        </p>
+      </div>
+    </div>
+  );
 
-  return <div className=''>{message()}</div>;
+  const ChatMessageTo = () => (
+    <div className='flex justify-end'>
+      <div className='m-2 p-2 border rounded-lg basis-9/12 flex-initial self-end'>
+        <p className='text-lg'>{chat.message}</p>
+        <p className='text-xs italic text-gray-500 float-right'>
+          {formatDistance(chat.sendAt, new Date(), {
+            addSuffix: true,
+          })}
+        </p>
+      </div>
+    </div>
+  );
+
+  const messages = !chat.isResponse ? <ChatMessageFrom /> : <ChatMessageTo />;
+
+  return <div className=''>{messages}</div>;
 };
 
 export default ChatItem;
