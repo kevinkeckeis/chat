b422e289cabf6d9e990134b027715a016b345823
Kevin Keckeis <35276744+kevinkeckeis@users.noreply.github.com>
2022-06-29 13:29:57 +0200

    remove debug logs

--- src/features/chats/ChatList.js
+++ src/features/chats/ChatList.js
@@ -6,9 +6,7 @@ import ContactItem from '../../components/ContactItem';
 
 const ChatList = () => {
   const uniqUserIds = useSelector(selectUniqUserIds);
-  console.log('---', uniqUserIds);
   const users = useSelector(selectUsersByIds(uniqUserIds));
-  console.log(users);
 
   const contactItems = users.map((user) => (
     <ContactItem key={user.id} user={user} />
